d19e61de7b0110b8ccd3a46281af482dae608a67
Mig Gebril <mig@tanius.net>
2019-07-15 20:50:29 -0400

    spell check tokens

--- esgtopics.py
+++ esgtopics.py
@@ -18,6 +18,8 @@ from esgtopickey import ESGTopicKey
 import numpy as np
 import pandas as pd
 
+from symspellpy.symspellpy import SymSpell, Verbosity 
+
 from gensim.utils import simple_preprocess
 from gensim.parsing.preprocessing import STOPWORDS
 
@@ -26,9 +28,8 @@ from nltk.stem.porter import *
 
 '''configures file logging for error and debug info'''
 def initLogger():
-    logging.basicConfig(filename='logfile.log', level=logging.WARNING, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+    logging.basicConfig(filename='logfile.log', level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
     logging.basicConfig(filename='errorfile.log', level=logging.ERROR, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
-
 
 '''remotely downloads nltk.wordnet -- requires network connection'''
 def loadPackages():
@@ -48,6 +49,7 @@ def loadPaths():
 
     return dirFiles
 
+'''writes to a csv file all tokens per company and year'''
 def writeTokensToFile(company, year, tokens, filePath):
     rows = [[company, year, t.token, str(t.count)] for t in tokens]
     with open(filePath, encoding='utf-8', mode='a+') as outputCsv:
@@ -67,6 +69,29 @@ def preprocess(text):
         if token not in gensim.parsing.preprocessing.STOPWORDS and len(token) > 2:
             result.append(lemmatize_stemming(token))
     return result
+
+def spellCheck(tokens, maxEditDistance=4, prefixLen=7):
+    symSpell = SymSpell(maxEditDistance, prefixLen)
+    dictionaryPath = os.path.join(os.path.dirname(__file__), "frequency_dictionary_en_82_765.txt")
+    
+    if not symSpell.create_dictionary(dictionaryPath):
+        logging.debug(f"File not found: {dictionaryPath}")
+        return
+
+    corrections = []
+    for idx, word in enumerate(tokens):
+        max_edit_distance_lookup = 3
+        suggestion_verbosity = Verbosity.TOP  # TOP, CLOSEST, ALL
+        suggestion = symSpell.lookup(word, suggestion_verbosity, maxEditDistance)
+        if len(suggestion) > 0:
+            corrections.append(suggestion[0].term)
+            print(f"Spell correction: {word} {suggestion[0].term}")
+        else:
+            corrections.append(word)
+            print(f"No suggestions: {word}")
+
+    return corrections
+
 
 ''' :returns each topic in a company's report along with their frequency as a list of ESGTopic'''
 def countTopics(topicKey, topics):
@@ -102,6 +127,8 @@ if __name__ == '__main__':
         with open(filePath, encoding='utf-8', mode='r') as file:
             rawText = file.read()
             tokens = preprocess(rawText)
+            tokens = spellCheck(tokens)
+
             print(f'\r\nTokenized. Prepare DB record.\r\n')
 
             fileName = ntpath.basename(filePath)
